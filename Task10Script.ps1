a419d240eda87d128792d0b7b6ae312deefbb856
Anas Mohamed Aziz Najjar <njourawebdev@gmail.com>
2024-01-16 08:41:19 +0100

    Create Task10Script.ps1

---
+++ Task10Script.ps1
@@ -0,0 +1,73 @@
+Start-Sleep -s 13
+
+# Specify the path to the credentials file
+$credentialsFilePath = "C:\Powershelling_20_WebApp\Credentials.json"
+
+# Specify the current task level
+$taskLevel = 10
+
+# Define the user for 'user2'
+$userName10 = "user10"
+
+$password10 = "NjouraUnique"
+
+# Convert the password to a secure string
+$securePassword10 = ConvertTo-SecureString $password10 -AsPlainText -Force
+
+# Create the new local user 'user2'
+Set-LocalUser -Name $userName10 -Password $securePassword10
+
+# Get Readme file 
+$Readme = (Get-Content -Path "C:\Users\user10\Desktop\README.txt" -Raw).Trim()
+
+# Function to check if credentials already exist in JSON file
+function CredentialsExist($credentialsArray, $newCredentials) {
+    foreach ($cred in $credentialsArray) {
+        if ($cred.UserName -eq $newCredentials.UserName -and $cred.TaskLevel -eq $newCredentials.TaskLevel) {
+            return $cred
+        }
+    }
+    return $null
+}
+
+
+
+# Load existing credentials from JSON file
+$existingCredentials = @()
+if (Test-Path $credentialsFilePath) {
+    $existingCredentials = Get-Content $credentialsFilePath | ConvertFrom-Json
+}
+
+# New credentials
+$newCredentials = @{
+    TaskLevel = 10 
+    UserName = "user10"
+    Password = $password10
+    Readme = $Readme
+}
+
+# Check if credentials already exist
+$existingCredential = CredentialsExist $existingCredentials $newCredentials
+
+if ($existingCredential) {
+    Write-Host "Credentials already exist. Updating password."
+    # Update password if it has changed
+    if ($existingCredential.Password -ne $newCredentials.Password) {
+        $existingCredential.Password = $newCredentials.Password
+        # Convert to JSON and save to file
+        $existingCredentials | ConvertTo-Json | Set-Content -Path $credentialsFilePath
+        Write-Host "Credentials updated in $credentialsFilePath."
+    } else {
+        Write-Host "Password unchanged. No update needed."
+    }
+} else {
+    Write-Host "Adding new credentials."
+    # Add new credentials
+ $existingCredentials = @($existingCredentials) + @($newCredentials)
+
+
+
+    # Convert to JSON and save to file
+    $existingCredentials | ConvertTo-Json | Set-Content -Path $credentialsFilePath
+    Write-Host "Credentials saved to $credentialsFilePath."
+}
